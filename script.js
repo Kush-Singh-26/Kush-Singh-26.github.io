097e8fafb84dcd0f487892ce069420e54df2883f
Kush-Singh-26 <kush26cs@gmail.com>
2025-03-09 02:46:26 +0530

    updated project

--- script.js
+++ script.js
@@ -26,35 +26,3 @@ function disableDarkMode() {
     body.classList.remove('dark');
     darkToggleBtn.innerHTML = '<span class="material-icons">dark_mode</span>';
 }
-
-function createMobileNav() {
-    const navbar = document.querySelector('.navbar');
-    const navLinks = document.querySelector('.nav-links');
-    
-    const mobileMenuBtn = document.createElement('button');
-    mobileMenuBtn.classList.add('mobile-menu-btn');
-    mobileMenuBtn.innerHTML = '<span class="material-icons">menu</span>';
-    
-    navbar.insertBefore(mobileMenuBtn, darkToggleBtn);
-    
-    mobileMenuBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        
-        if (navLinks.classList.contains('active')) {
-            mobileMenuBtn.innerHTML = '<span class="material-icons">close</span>';
-        } else {
-            mobileMenuBtn.innerHTML = '<span class="material-icons">menu</span>';
-        }
-    });
-    
-    // Close mobile menu when a link is clicked
-    const navLinksArray = document.querySelectorAll('.nav-links a');
-    navLinksArray.forEach(link => {
-        link.addEventListener('click', () => {
-            navLinks.classList.remove('active');
-            mobileMenuBtn.innerHTML = '<span class="material-icons">menu</span>';
-        });
-    });
-}
-
-//
